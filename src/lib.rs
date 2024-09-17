0304285c4bb4ca03f28edfeb5a4ee59345bffd4a
ElectroMutex <97535877+SkillfulElectro@users.noreply.github.com>
2024-09-17 20:45:28 +0330

    v0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,19 @@
 /*!
-this crate just provides simple macro to implement safe struct which 
-drops a value when it goes out of the scope
-*/
+  this crate just provides simple macro to implement safe struct which 
+  drops a value when it goes out of the scope
+  */
+
+#![no_std]
+
 
 #[macro_export]
 /// call this macro this way : 
 /// safetruct!(visibility of your struct , name_of_that_value : its type , name_of_the_allocator_func , name_of_the_deallocator_func); 
 ///
 /// allocator func must get no para and return the type which is setted 
-/// deallocator must get *mut *mut pointer to that type 
+/// deallocator must get *mut *mut pointer to that type
+///
+/// if you deref the struct , it will use the inner value 
 macro_rules! safetruct {
 
     ($visibility:ident , $name:ident, $valuename:ident: $type:ty, $user_defined_alloc_func:ident, $user_defined_dealloc_func:ident) => {
@@ -39,5 +44,20 @@ macro_rules! safetruct {
                 }
             }
         }
+
+        impl core::ops::Deref for $name {
+            type Target = $type;
+
+            fn deref(&self) -> &Self::Target {
+                &self.$valuename
+            }
+        }
+
+        impl core::ops::DerefMut for $name {
+            fn deref_mut(&mut self) -> &mut Self::Target {
+                &mut self.$valuename
+            }
+        }
+
     };
 }
